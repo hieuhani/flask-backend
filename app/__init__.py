21a69147c012101402e743b0a7d8c0028bafbc92
Hieu Tran <hieutran.fu@gmail.com>
2018-05-26 19:22:49 +0700

    Remove graphql endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from flask_graphql import GraphQLView
 from flask_sqlalchemy import SQLAlchemy
 from config import config
-from app import graphql
 
 db = SQLAlchemy()
 
@@ -13,14 +11,5 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
-
-    app.add_url_rule(
-        '/graphql',
-        view_func=GraphQLView.as_view(
-            'graphql',
-            schema=graphql.schema,
-            graphiql=True,
-        )
-    )
 
     return app
